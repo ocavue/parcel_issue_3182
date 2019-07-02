cb01d3dd5045ad611e54c8192826616fd5aaec48
ocavue <ocavue@gmail.com>
2019-07-02 12:12:42 +0800

    add code

---
+++ App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div> Hello {{bundler}} </div>
+</template>
+
+<script>
+import Vue from "vue";
+
+export default Vue.extend({
+  data() {
+    return {
+      bundler: "Parcel"
+    };
+  }
+});
+</script>
---
+++ index.js
@@ -0,0 +1,6 @@
+import Vue from 'vue';
+import App from './App.vue';
+
+console.log('index.js is loaded')
+
+new Vue(App).$mount('#app')
